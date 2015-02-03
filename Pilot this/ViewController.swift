71b8cac92fe3cfc2c689ebf75073223457a4eca9
Fernando Rios Garate <yayo@MacBook-Pro.local>
2015-02-03 12:46:13 -0500

    Initial Commit

---
+++ Pilot this/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pilot this
+//
+//  Created by Fernando Rios Garate on 2/3/15.
+//  Copyright (c) 2015 BlueKoala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
