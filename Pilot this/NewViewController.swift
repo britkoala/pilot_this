447096447042caf28aa3779803b947b750312ccc
Gustavo Villar <Guti@Gustavos-MacBook-Pro.local>
2015-03-28 10:39:36 -0500

    Added: Keyboard dismissed on NewViewController Added: TextFields are no longer hidden by the Keyboard on smaller devices (NewViewController)

--- Pilot this/NewViewController.swift
+++ Pilot this/NewViewController.swift
@@ -9,31 +9,122 @@
 import UIKit
 import CoreData
 
-class NewViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class NewViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
     
-    
+    // MARK: Outlets
     @IBOutlet weak var pickedImage: UIImageView!
     @IBOutlet weak var productName: UITextField!
     @IBOutlet weak var productPrice: UITextField!
     @IBOutlet weak var selectImageButton: UIButton!
+    @IBOutlet weak var descriptionView: UIView! // Container of both Text Fields
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
     
+    
+    // MARK: View Controller Life Cicle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
         // Do any additional setup after loading the view.
+        registerForKeyboardNotifications()
         
         // Change placeholder color
-        productName.attributedPlaceholder = NSAttributedString(string: "Product Name", attributes: [NSForegroundColorAttributeName: UIColor(red: 185/255, green: 185/255, blue: 185/255, alpha: 1)])
-        productPrice.attributedPlaceholder = NSAttributedString(string: "Price (Aprox.)", attributes: [NSForegroundColorAttributeName: UIColor(red: 185/255, green: 185/255, blue: 185/255, alpha: 1)])
+        let placeHolderColor = UIColor(white: 185/255, alpha: 1)
+        productName.attributedPlaceholder = NSAttributedString(string: "Product Name", attributes: [NSForegroundColorAttributeName: placeHolderColor])
+        productPrice.attributedPlaceholder = NSAttributedString(string: "Price (Aprox.)", attributes: [NSForegroundColorAttributeName: placeHolderColor])
+        
+        // Dismiss Keyboard on Touch
+        let keyboardDismisalGesture = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        self.view.addGestureRecognizer(keyboardDismisalGesture)
+
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: Keyboard Notifications
+    func registerForKeyboardNotifications() {
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardDidChangeFrame:", name: UIKeyboardWillChangeFrameNotification, object: nil)
+        
+//        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardDidShowNotification, object: nil)
+        
+//        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func keyboardDidChangeFrame(notification: NSNotification){
+        println("-------------------")
+        
+        let info = notification.userInfo!
+        let keyboardSize = (info[UIKeyboardFrameEndUserInfoKey] as NSValue).CGRectValue().size
+        println("Keyboard size: \(keyboardSize)")
+        
+        let viewHeight = view.frame.height
+        println("View height: \(viewHeight)")
+        
+        let visibleHeight = viewHeight - keyboardSize.height
+        println("Visible height: \(visibleHeight)")
+        
+        let contentHeight = pickedImage.frame.origin.y + pickedImage.frame.height
+        println("Content height: \(contentHeight)")
+        
+        // Move the content up if its not visible
+        if visibleHeight < contentHeight {
+            let delta = contentHeight - visibleHeight
+            println("Delta: \(delta)")
+            
+            bottomConstraint.constant = -delta // Move descriptionView up
+            
+            // Animate constraint change
+            UIView.animateWithDuration(0.5) {
+                self.view.layoutIfNeeded()
+            }
+        } else if bottomConstraint.constant != 0 {
+            bottomConstraint.constant = 0 // Return descriptionView to its original place
+            
+            // Animate constraint change
+            UIView.animateWithDuration(0.5) {
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        let info = notification.userInfo!
+        let keyboardSize = (info[UIKeyboardFrameBeginUserInfoKey] as NSValue).CGRectValue().size
+        
+        let visibleHeight = view.frame.height - keyboardSize.height
+        
+        let contentHeight = descriptionView.frame.origin.y + descriptionView.frame.height
+        
+        // Move the content up if its not visible
+        if visibleHeight < contentHeight {
+            let delta = contentHeight - visibleHeight
+            println("Delta: \(delta)")
+            
+            bottomConstraint.constant = delta // Move descriptionView up
+            
+            // Animate constraint change
+            UIView.animateWithDuration(1) {
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+
+    func keyboardWillHide(notification: NSNotification) {
+        if bottomConstraint.constant != 0 {
+            bottomConstraint.constant = 0 // Return descriptionView to its original place
+            
+            // Animate constraint change
+            UIView.animateWithDuration(0.5) {
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+    
+    // MARK: Pick Image
     @IBAction func pickImage(sender: AnyObject) {
         
         var image = UIImagePickerController()
@@ -45,12 +136,25 @@ class NewViewController: UIViewController, UIImagePickerControllerDelegate, UINa
         
     }
     
+    // MARK: Image Picker Controller Delegate
     func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
         self.dismissViewControllerAnimated(true, completion: nil)
         pickedImage.image = image
-        selectImageButton.setTitle("", forState: .Normal)
+        selectImageButton.setTitle("", forState: .Normal) // Hide button's text
     }
     
+    // MARK: Text Field Delegate
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(false)
+    }
+    
+    
+    // MARK: Start Pilot
     
     @IBAction func startPilot(sender: AnyObject) {
         // Validation: name required, image and price optional
